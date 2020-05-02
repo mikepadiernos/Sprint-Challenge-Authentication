b3e5893612bf2ea866ce004210965d70671bb975
Michael Padiernos <mike.padiernos@gmail.com>
2020-05-01 21:40:11 -0400

    [FEATURE] Middleware / coded
    - middleware for authentication coded

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,18 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, "", (error, decodedToken) => {
+      token
+        ? res.status(401).json({ you: 'shall not pass!' })
+        : req.decodedToken = decodedToken && next();
+    })
+  } else {
+    res.status(401).json({ you: 'can\'t touch this!' });
+  }
 };
